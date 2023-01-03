99d4df0b47addd94760b4642c3aeed6395dd995b
omarzouaoui <zouaouiomar80@gmail.com>
2023-01-03 14:33:01 +0100

    SignIn Component

--- flutter_auth/lib/components/input_text_filed.dart
+++ flutter_auth/lib/components/input_text_filed.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
 class InputTextField extends StatelessWidget {
-  const InputTextField({super.key});
+
+  final controller;
+  final String hintText;
+  final bool obscureText;
+
+  const InputTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+    });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
                  child: TextField(
+                  controller: controller,
+                  obscureText: obscureText,
                   decoration: InputDecoration(
                     enabledBorder: const OutlineInputBorder(
                       borderSide: BorderSide(
@@ -23,6 +35,10 @@ class InputTextField extends StatelessWidget {
 
                     fillColor: Colors.grey.shade100,
                     filled: true,
+                    hintText: hintText,
+                    hintStyle: TextStyle(
+                      color: Colors.grey.shade500
+                    ),
                   ),
               ),
                );
--- flutter_auth/lib/pages/login_page.dart
+++ flutter_auth/lib/pages/login_page.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/components/input_text_filed.dart';
+import 'package:flutter_auth/components/my_button.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  //text controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  //sign in
+  void SignUserIn(){}
 
   @override
   Widget build(BuildContext context) {
@@ -23,18 +31,85 @@ class LoginPage extends StatelessWidget {
               Text(
                 "Welcome back you've been missed",
                 style: TextStyle(
-                  color: Colors.grey.shade800,
+                  color: Colors.grey.shade600,
                   fontSize: 15,
                 ),
               ),
-              const SizedBox(height: 25,),
+              const SizedBox(height: 25.0,),
               //user name textfield
-              const InputTextField(),
+              InputTextField(
+                controller: usernameController,
+                hintText: "Username",
+                obscureText: false,
+              ),
 
-              const SizedBox(height: 20.0,),
+              const SizedBox(height: 10.0,),
       
               //password textfield
-              const InputTextField(),
+              InputTextField(
+                controller: passwordController,
+                hintText: "Password",
+                obscureText: true,
+              ),
+              
+              const SizedBox(height: 10.0,),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 26.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Forget Password?",
+                      style: TextStyle(
+                        color: Colors.grey.shade600 
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 25.0,),
+
+              MyButton(
+                onTap: SignUserIn,
+              ),
+
+              const SizedBox(height: 50.0,),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        "Or continue with",
+                        style: TextStyle(
+                          color: Colors.grey.shade800
+                        ),
+                        ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              
+
+
+
       
               //forgot password
       
