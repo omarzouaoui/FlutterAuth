9c78465ac21e0688bb89900186e5a916bc2cf76e
omarzouaoui <zouaouiomar80@gmail.com>
2023-01-03 12:01:46 +0100

    starting on login_page

---
+++ flutter_auth/lib/pages/login_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade400,
+      body: Column(
+        children: [
+
+          //logo
+
+
+          //welcome
+
+
+        ],
+      )
+    );
+  }
+}
